cf289ec4688992b8386afec0850d0f6fc2de8aa1
Caio Marques <caiohenrique1146@gmail.com>
2024-09-02 16:02:50 -0300

    Creating form home to Thoughts

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -3,15 +3,116 @@ const User = require('../models/User')
 
 class ThoughtController {
   static async showThoughts(req, res) {
-    res.render('thoughts/home')
+
+    const thoughtsData = await Thought.findAll({
+      include: User,
+    })
+
+    const thoughts = thoughtsData.map((result) => result.get({plain: true}))
+
+    res.render('thoughts/home', {thoughts})
   }
 
   static async dashboard(req, res) {
-    res.render('thoughts/dashboard')
+    const userId = req.session.userId
+
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Thought,
+      plain: true,
+    })
+
+    if(!user) {
+      res.redirect('/login')
+    }
+
+    const thoughts = user.Thoughts.map((result) => result.dataValues)
+
+    let emptyThoughts = false
+
+    if(thoughts.length === 0) {
+      emptyThoughts = true
+    }
+
+    res.render('thoughts/dashboard', { thoughts, emptyThoughts })
   }
 
   static createThought(req, res) {
     res.render('thoughts/create')
+  }
+
+  static async createThoughtSave(req, res) {
+    const thought = {
+      title: req.body.title,
+      UserId: req.session.userId
+    }
+
+    try {
+
+      await Thought.create(thought)
+
+      req.flash('message', 'Thought created with success!')
+  
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+
+      })
+    } catch (error) {
+      console.log('An unexpected error occured:', error)
+    }
+  }
+
+  static async removeThought(req, res) {
+
+    const id = req.body.id;
+    const userid = req.session.userId;
+
+    try {
+
+      await Thought.destroy({ where: {id: id, userid: userid} })
+
+      req.flash('message', 'Thought deleted with success!')
+      req.session.save(() => {
+        res.redirect('/thoughts/dashboard')
+      })
+      
+    } catch (error) {
+      console.log('Un expected error ocurred', error)
+    }
+
+  }
+  
+  static async updateThought(req, res) {
+    const id = req.params.id;
+
+    const thought = await Thought.findOne({ where: { id:id }, raw: true})
+
+    res.render('thoughts/edit', { thought })
+  }
+
+  static async updateThoughtSave(req, res) {
+
+    const id = req.body.id;
+
+    const thought = {
+      title: req.body.title
+    }
+
+    try {
+      
+      await Thought.update(thought, { where: { id: id } })
+
+    req.flash('message', 'Thought deleted with success!')
+    req.session.save(() => {
+      res.redirect('/thoughts/dashboard')
+    })
+    
+    } catch (error) {
+      console.log('An expected error ocurred', error)
+    }
+
   }
 }
 
